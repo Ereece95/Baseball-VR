3d16998dca5c128ca9b3622578bcfa8fba5aeb67
amccracken <amccracken@ERC117-03>
2016-09-29 15:52:32 -0400

    attached ball -AM & DH

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,15 +6,12 @@ public class Ball : MonoBehaviour {
     public GameObject hand;
 	// Use this for initialization
 	void Start () {
-        var ballTransform = ball.transform;
-        ballTransform.parent = transform.Find("R Hand");
-        ballTransform.localPosition = Vector3.zero;
-        ballTransform.localRotation = Quaternion.identity;
-        ballTransform.localScale = Vector3.one;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
+        ball.transform.position = hand.transform.position;
 
     }
 }

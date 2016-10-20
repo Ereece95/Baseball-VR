9328172c5607df88af8dedc153948af825fcc392
amccracken <amccracken@ERC117-01>
2016-10-20 15:43:27 -0400

    Pitch Script

--- Assets/Scripts/NextPitch.cs
+++ Assets/Scripts/NextPitch.cs
@@ -5,6 +5,8 @@ public class NextPitch : MonoBehaviour {
 
     public void LoadLevel(int level)
     {
+        level = 1;
+        Object.DontDestroyOnLoad
         Application.LoadLevel(level);
     }
 }

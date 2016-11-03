5df1da7cd5ae296131c3d2243979921378f93797
towens182 <TOwens@student.dwc.edu>
2016-11-03 12:06:40 -0400

    Work

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,7 +6,12 @@ public class Ball : MonoBehaviour
     public GameObject ball;
     public GameObject hand;
     private TrailRenderer trail;
-    int x=0;
+    int x = 0;
+    public Transform target;
+    public Transform[] path;
+    public Transform[] pathArray;
+    public float speed, throws;
+
 
     void Awake()
     {
@@ -14,14 +19,15 @@ public class Ball : MonoBehaviour
     }
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         trail.enabled = false;
     }
-	
-	// Update is called once per frame
-	void Update ()
+    int i = 0;
+    // Update is called once per frame
+    void Update()
     {
+
         //an if statment to have the ball released at a certain time
         if (x < 70)
         {
@@ -31,9 +37,26 @@ public class Ball : MonoBehaviour
         }
         else
         {
-            //when x reaches a spesific value it enables the tral and moves the ball
+            float step = speed * Time.deltaTime;
             trail.enabled = true;
-            ball.transform.Translate(0f, -0.03125f, -0.25f);
+            //when x reaches a spesific value it enables the tral and moves the ball
+
+
+            if (ball.transform.position == path[i].position)
+            {
+                i++;
+            }
+
+            transform.position = Vector3.MoveTowards(ball.transform.position, path[i].position, step);
+            if(Input.GetMouseButtonDown(0))
+            {
+                transform.position = Vector3.zero;
+            }
+
+
+
+
+            //ball.transform.Translate(0f, -0.03125f, -0.25f);
 
         }
         //increments x to determine when to relase the ball

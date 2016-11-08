8665cf68a5db31f4a5e81e88dc8fd060cb5aac87
Executive5 <tjg0512@gmail.com>
2016-11-08 15:49:12 -0500

    Started on File reader

---
+++ Assets/MonsterLove/StateMachine/StatsScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+
+public class StatsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+public class Pitcher
+{
+    StreamReader reader = new StreamReader(File.OpenRead("FileName.csv"));
+    Dictionary<string, float> variables = new Dictionary<string, float>();
+
+    public Pitcher()
+    {
+        while (!reader.EndOfStream)
+        {
+            string t = reader.ReadLine();
+            variables[t] = float.MinValue;
+        }
+    }
+    
+    
+};

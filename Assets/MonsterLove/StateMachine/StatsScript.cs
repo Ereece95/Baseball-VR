23d4301c57a78aa60d264e21c13a407accba5ea5
Executive5 <tjg0512@gmail.com>
2016-11-10 11:55:35 -0500

    ReadFromFile work

--- Assets/MonsterLove/StateMachine/StatsScript.cs
+++ Assets/MonsterLove/StateMachine/StatsScript.cs
@@ -5,33 +5,58 @@ using System;
 using System.Linq;
 using System.Text;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-
-public class StatsScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
-public class Pitcher
+namespace CsvToCSharpClass.Library
 {
-    StreamReader reader = new StreamReader(File.OpenRead("FileName.csv"));
-    Dictionary<string, float> variables = new Dictionary<string, float>();
-
-    public Pitcher()
+    public class Pitcher
     {
-        while (!reader.EndOfStream)
+        public static string CSharpClassCodeFromCsvFile(string filePath, string delimiter = ",", string classAttribute = "", string propertyAttribute = "")
         {
-            string t = reader.ReadLine();
-            variables[t] = float.MinValue;
+            //if (string.IsNullOrWhiteSpace(propertyAttribute) == false)
+               // propertyAttribute += "\n\t";
+           // if (string.IsNullOrWhiteSpace(propertyAttribute) == false)
+               // classAttribute += "\n";
+
+            string[] lines = File.ReadAllLines(filePath);
+            string[] columnNames = lines.First().Split(',').Select(str => str.Trim()).ToArray();
+            string[] data = lines.Skip(1).ToArray();
+
+            string className = Path.GetFileNameWithoutExtension(filePath);
+            // use StringBuilder for better performance
+            string code = String.Format("{0}public class {1} {{ \n", classAttribute, className);
+
+            for (int columnIndex = 0; columnIndex < columnNames.Length; columnIndex++)
+            {
+                var columnName = Regex.Replace(columnNames[columnIndex], @"[\s\.]", string.Empty, RegexOptions.IgnoreCase);
+                if (string.IsNullOrEmpty(columnName))
+                    columnName = "Column" + (columnIndex + 1);
+                //code += "\t" + GetVariableDeclaration(data, columnIndex, columnName, propertyAttribute) + "\n\n";
+            }
+
+            code += "}\n";
+            return code;
         }
+
+    };
+}
+public class Stat
+{
+    string name;
+    float stat;
+
+    public Stat(string n, float s)
+    {
+        name = n;
+        stat = s;
     }
-    
-    
+
+    float getStat()
+    {
+        return stat;
+    }
+    string getName()
+    {
+        return name;
+    }
 };

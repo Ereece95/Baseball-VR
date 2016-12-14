fa36d8095f934dab004617bec41e172fea6da8d5
Aidan <Aidan@DESKTOP-7239PHC>
2016-12-14 15:50:08 -0500

    commit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -151,8 +151,6 @@ public class Ball : MonoBehaviour
                 if (hit)
                 {
                     float dist = Vector3.Distance(start.position, ball.transform.position);
-
-
                     double dist2 = System.Convert.ToDouble(dist);
                     dist2 = System.Math.Round(dist2, 2);
                     DistDisplay.text = "Distance: " + (dist2.ToString()) + " m";

21cff9fc2ece9833bf051d829bf7879743603438
Administrator <Administrator@GIGI>
2016-12-19 14:00:28 -0500

    video

---
+++ Assets/Mark/videoScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+//public class videoScript : MonoBehaviour {
+
+//    public MovieTexture movie;
+   
+//	// Use this for initialization
+//	void Start ()
+//    {
+//        Plane.GetComponent<Renderer>().mainTexture = movie;
+//        movie.Play();
+        //***incorporate the video compare button here***
+//	}
+	
+//}

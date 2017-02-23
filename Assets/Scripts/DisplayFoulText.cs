0238dcc98fac92de8628e23ba18625f7c9d6223e
Aidan <Aidan@DESKTOP-7239PHC>
2017-02-22 21:14:04 -0500

    Disabled pitch type when foul ball text is showing.

--- Assets/Scripts/DisplayFoulText.cs
+++ Assets/Scripts/DisplayFoulText.cs
@@ -6,16 +6,20 @@ public class DisplayFoulText : MonoBehaviour {
 
     public Canvas displayCanvas;
     public Text pitchText;
+    private DisplayPitch dsplyPitch;
     private GameController gc;
     bool foul; // flag for when ball is foul
+    bool disabledPitchType;
 
     void Start()
     {
         // find canvas and disable on start
         displayCanvas = GameObject.Find("FoulBallCanvas").GetComponent<Canvas>();
         gc = GameObject.Find("GameController").GetComponent("GameController") as GameController;
+        dsplyPitch = GameObject.Find("DisplayPitchButton").GetComponent("DisplayPitch") as DisplayPitch;
         displayCanvas.enabled = false;
         foul = true;
+        disabledPitchType = false;
     }
 
     /// <summary>
@@ -27,11 +31,21 @@ public class DisplayFoulText : MonoBehaviour {
         if (foul && (gc.GetState() == States.WaitForInput))
         {
             displayCanvas.enabled = true;
+            if (dsplyPitch.isHidden == false)
+            {
+                disabledPitchType = true;
+                dsplyPitch.displayPitchType();
+            }
         }
         else
         {
             displayCanvas.enabled = false;
             foul = false;
+            if (disabledPitchType == true)
+            {
+                disabledPitchType = false;
+                dsplyPitch.displayPitchType();
+            }
         }
     }
     public void displayFoulText()

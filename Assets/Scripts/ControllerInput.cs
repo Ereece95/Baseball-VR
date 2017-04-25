5cee490dc4fe861934512463f83f3135af259843
Travis <Travis@DESKTOP-OHSC72K>
2017-04-24 20:28:49 -0500

    I think we are done

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -54,7 +54,7 @@ public class ControllerInput : MonoBehaviour
     {
         Debug.Log("In pad clicked. e.padY = " + e.padY.ToString());
         Debug.Log("State = " + gc.GetState());
-        if (e.padY < -0.5f && gc.GetState() == States.WaitForInput)
+        if (e.padY < -0.5f && gc.GetState() == States.WaitForInput || e.padY < -0.5f && gc.GetState() == States.OptionsDisplay)
         {
             gc.HandlePadPressed();
         }
